e6f99d566f253e0fce7dd4427051bcc0ccaa78f1
Paul Berg <hello@paulrberg.com>
2019-02-26 19:14:41 +0000

    Add cyclical dependencies

---
+++ contracts/A.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./B.sol";
+
+contract A {
+  
+    uint256 public a;
+
+    constructor() public {
+        a = 100;
+    }
+}
---
+++ contracts/B.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./A.sol";
+
+contract B {
+  
+    uint256 public b;
+
+    constructor() public {
+        b = 121;
+    }
+}
